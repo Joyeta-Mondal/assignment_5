44e4041433dd8fc5f0752abe285b7eb1736cf391
Joyeta-Mondal <dipannitakotha2019@gmail.com>
2024-09-25 08:09:41 +0530

    added remaining sections and debug

--- js/script.js
+++ js/script.js
@@ -17,45 +17,17 @@ function toggleBtnAct(activeId, inactiveId) {
     document.getElementById(inactiveId).classList.remove('bg-lime', 'text-text-primary', 'border-lime');
     document.getElementById(inactiveId).classList.add('border', 'border-gray-500', 'text-black');
 }
-// donation activities
-function donation(cardID){
-    const donationIn = document.getElementById(`donationIn${cardID}`);
-    const donationAmt = parseFloat(donationIn.value);
-
-    if(isNaN(donationAmt) || donationAmt <= 0){
-        alert('Please enter a valid amount for donation!');
-        return;
-    }
-
-    if(donationAmt > balance){
-        alert('Sorry! Insufficient balance. Try to donate an amount within your balance.');
-        return;
-    }
-    
-    // donated amount deduction
-    balance -= donationAmt ;
-    document.getElementById('balanceAvl').textContent=balance;
-
-    // updation of donation
-    const currDonationAmt = document.getElementById(`donation${cardID}`);
-    const newDonationAmt = parseFloat(currDonationAmt.textContent) + donationAmt;
-    currDonationAmt.textContent= newDonationAmt;
-
-    addToAmtHistory(donationAmt, cardID);
-    donationIn.value= '';
-    donationModal.showModal();
-}
-function addToAmtHistory(donationAmt, cartNo) {
+function addToAmtHistory(donationAmt, cartId) {
     const historyLt = document.getElementById('historyLt');
     const timestamp = new Date().toString();
     let message;
 
 //donation msg as per card id
-    if (cartNo === 1) {
+    if (cartId === 1) {
         message = 'Flood Relief in Noakhali, Bangladesh';
-    } else if (cartNo === 2) {
+    } else if (cartId === 2) {
         message = 'Flood Relief in Feni, Bangladesh';
-    } else if (cartNo === 3) {
+    } else if (cartId === 3) {
         message = 'Aid for Injured in the Quota Movement, Bangladesh';
     }
 
@@ -74,3 +46,60 @@ function addToAmtHistory(donationAmt, cartNo) {
     // insertion of the transaction card as HTML into the history section
     historyLt.insertAdjacentHTML('beforeend', transactionCard);
 }
+// donation activities
+function donation(cardId){
+    const donationIn = document.getElementById(`donationIn${cardId}`);
+    const donationAmt = parseFloat(donationIn.value);
+
+    if(isNaN(donationAmt) || donationAmt <= 0){
+        alert('Please enter a valid amount for donation!');
+        return;
+    }
+
+    if(donationAmt > balance){
+        alert('Sorry! Insufficient balance. Try to donate an amount within your balance.');
+        return;
+    }
+    
+    // donated amount deduction
+    balance -= donationAmt ;
+    document.getElementById('balanceAvl').textContent=balance;
+
+    // updation of donation
+    const currDonationAmt = document.getElementById(`donation${cardId}`);
+    const newDonationAmt = parseFloat(currDonationAmt.textContent) + donationAmt;
+    currDonationAmt.textContent= newDonationAmt;
+
+    addToAmtHistory(donationAmt, cardId);
+    donationIn.value= '';
+    donationModal.showModal();
+}
+// function addToAmtHistory(donationAmt, cartId) {
+//     const historyLt = document.getElementById('historyLt');
+//     const timestamp = new Date().toString();
+//     let message;
+
+// //donation msg as per card id
+//     if (cartId === 1) {
+//         message = 'Flood Relief in Noakhali, Bangladesh';
+//     } else if (cartId === 2) {
+//         message = 'Flood Relief in Feni, Bangladesh';
+//     } else if (cartId === 3) {
+//         message = 'Aid for Injured in the Quota Movement, Bangladesh';
+//     }
+
+//     // transaction card
+//     const transactionCard = `
+//         <div class="p-4 md:p-8 border-gray-300 border rounded-xl mt-4 md:mt-8">
+//             <div class="space-y-3">
+//                 <h2 class="text-text-primary font-bold text-xl">
+//                     <span>${donationAmt}</span> Taka is Donated for <span>${message}</span>
+//                 </h2>
+//                 <p class="text-text-gray">Date: <span>${timestamp}</span></p>
+//             </div>
+//         </div>
+//     `;
+
+//     // insertion of the transaction card as HTML into the history section
+//     historyLt.insertAdjacentHTML('beforeend', transactionCard);
+// }
