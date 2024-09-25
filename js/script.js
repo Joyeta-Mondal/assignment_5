d08fd7cea3b5af801a88c4458db712c170096775
Joyeta-Mondal <dipannitakotha2019@gmail.com>
2024-09-25 08:12:37 +0530

    rearranged some code

--- js/script.js
+++ js/script.js
@@ -74,32 +74,3 @@ function donation(cardId){
     donationIn.value= '';
     donationModal.showModal();
 }
-// function addToAmtHistory(donationAmt, cartId) {
-//     const historyLt = document.getElementById('historyLt');
-//     const timestamp = new Date().toString();
-//     let message;
-
-// //donation msg as per card id
-//     if (cartId === 1) {
-//         message = 'Flood Relief in Noakhali, Bangladesh';
-//     } else if (cartId === 2) {
-//         message = 'Flood Relief in Feni, Bangladesh';
-//     } else if (cartId === 3) {
-//         message = 'Aid for Injured in the Quota Movement, Bangladesh';
-//     }
-
-//     // transaction card
-//     const transactionCard = `
-//         <div class="p-4 md:p-8 border-gray-300 border rounded-xl mt-4 md:mt-8">
-//             <div class="space-y-3">
-//                 <h2 class="text-text-primary font-bold text-xl">
-//                     <span>${donationAmt}</span> Taka is Donated for <span>${message}</span>
-//                 </h2>
-//                 <p class="text-text-gray">Date: <span>${timestamp}</span></p>
-//             </div>
-//         </div>
-//     `;
-
-//     // insertion of the transaction card as HTML into the history section
-//     historyLt.insertAdjacentHTML('beforeend', transactionCard);
-// }
